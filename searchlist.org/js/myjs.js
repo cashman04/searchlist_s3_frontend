844e47402462cc80d4aa042c186b052541c2a367
Dan Cash <dancash04@gmail.com>
2017-09-04 23:45:24 +0000

    Testing sed command to modify myjs(needs renaminggit status) file to change api url to reflect current branch

--- searchlist.org/js/myjs.js
+++ searchlist.org/js/myjs.js
@@ -32,7 +32,7 @@ function submitRequestAPI() {
   $("ul#city_list").empty()
   $.ajax({
   type: "POST",
-  url: 'https://api.searchlist.org/beta',
+  url: 'https://api.searchlist.org/REGION',
   data: JSON.stringify(data)
 });
 };
